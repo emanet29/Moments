bd677d1851ce0c7e1036790ecbf0fed740d71322
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-06-05 10:12:22 +0200

    ratio image

--- Moments/Controller/CollectionController.swift
+++ Moments/Controller/CollectionController.swift
@@ -41,7 +41,18 @@ class CollectionController: UIViewController, UICollectionViewDelegate, UICollec
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 150, height: 150)
+        
+        let image = photos[indexPath.item].image
+        let largeur = collectionView.frame.width / 2 - 30
+        let ratio = largeur / image.size.width
+        let width = image.size.width * ratio
+        let height = image.size.height * ratio
+        
+        
+        
+        
+        
+        return CGSize(width: width, height: height)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
